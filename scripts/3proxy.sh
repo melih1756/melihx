53df4294bc6ae580c2f279b420e82dc21912cd04
melih1756 <76849593+melih1756@users.noreply.github.com>
2021-01-08 16:32:38 +0300

    Update 3proxy.sh

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -8,7 +8,7 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/xlandgroup/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    wget https://raw.githubusercontent.com/melih1756/melihx/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
     mv /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/
     systemctl daemon-reload
     systemctl enable 3proxy
